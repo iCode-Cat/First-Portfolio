bd4cd6961fa068df22b0c344b25b31c2e49f0037
iCode-Cat <74076633+iCode-Cat@users.noreply.github.com>
2020-12-09 23:15:17 +0200

    Add files via upload

---
+++ DreamCodePortfolio/index.js
@@ -0,0 +1,9 @@
+
+$(".baloon-anim").click(function(){
+
+   
+    document.getElementById('role').style.animationName='role';
+    document.getElementById('background').style.animationName='red';
+    document.getElementByClassName('nav-bar').style.animationName='red';
+   
+})
